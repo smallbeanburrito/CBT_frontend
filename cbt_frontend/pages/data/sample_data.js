a109373c4eaa951e988e3ee0891be2d4d4df395c
Keith Cao <97124773+smallbeanburrito@users.noreply.github.com>
2024-07-07 23:09:39 -0400

    refactor sample_data

---
+++ cbt_frontend/pages/data/sample_data.js
@@ -0,0 +1,62 @@
+export const sample_data = [
+  {
+    amount: 12.53,
+    category: "Food",
+    bank: "TD",
+    date: "07/28/2023",
+  },
+  {
+    amount: 50.62,
+    category: "Transportation",
+    bank: "CIBC",
+    date: "07/25/2023",
+  },
+  {
+    amount: 32.51,
+    category: "Food",
+    bank: "RBC",
+    date: "07/24/2023",
+  },
+  {
+    amount: 90.07,
+    category: "Online",
+    bank: "CIBC",
+    date: "07/23/2023",
+  },
+  {
+    amount: 3.4,
+    category: "Misc.",
+    bank: "BMO",
+    date: "07/22/2023",
+  },
+  {
+    amount: 60.33,
+    category: "Grocery",
+    bank: "ScotiaBank",
+    date: "07/20/2023",
+  },
+  {
+    amount: 17.85,
+    category: "Retail",
+    bank: "CIBC",
+    date: "07/19/2023",
+  },
+  {
+    amount: 108.79,
+    category: "Grocery",
+    bank: "TD",
+    date: "07/18/2023",
+  },
+  {
+    amount: 16.25,
+    category: "Transportation",
+    bank: "BMO",
+    date: "07/17/2023",
+  },
+  {
+    amount: 32.4,
+    category: "Gas",
+    bank: "BMO",
+    date: "07/13/2023",
+  },
+];
